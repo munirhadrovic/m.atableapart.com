622b084a3ef138b37ab6a8e215ac5b57193f2aba
Emir <emiratesbp@gmail.com>
2015-04-20 13:22:09 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="4295 Bonita Beach Road"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Bonita Springs, FL 34134";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2392218540;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="slide"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
